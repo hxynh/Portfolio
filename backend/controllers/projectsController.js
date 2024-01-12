6614afc4882562997db30ef5bc34aa248091d00c
Nancy <nhoushop@outlook.com>
2024-01-12 04:25:22 -0500

    Added error handling middleware

--- backend/controllers/projectsController.js
+++ backend/controllers/projectsController.js
@@ -1,10 +1,17 @@
+const asyncHandler = require('express-async-handler')
 
 // @desc    Get Projects
 // @route   GET /api/projects
 // @access  Private
-const getProjects = (req, res) => {
+const getProjects = asyncHandler (async (req, res) => {
+    const {id} = req.body
+    if(!id) {
+        res.status(400)
+        throw new Error('Project not found')
+    }
+
     res.status(200).json({message: 'Get Projects'})
-}
+})
 
 module.exports = {
     getProjects
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require ('express')
 const dotenv = require ('dotenv').config()
 const port = process.env.PORT || 5000
-
+const {errorHandler} = require ('./middleware/errorMiddleware')
 const app = express()
 
+// Adding middleware to use body data
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/projects', require('./routes/projectsRoute'))
+
+// Overwrite the default handler with custom errorHandler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
