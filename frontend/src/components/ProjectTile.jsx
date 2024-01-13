df3591645600511d749a39d072ae0d17197a8550
Nancy <nhoushop@outlook.com>
2024-01-13 04:44:46 -0500

    Removed dummy data for Project Tiles, connected frontend to backend with Axios to display Project details fetched from DB

--- frontend/src/components/ProjectTile.jsx
+++ frontend/src/components/ProjectTile.jsx
@@ -1,20 +1,17 @@
 import classes from "./styles/ProjectTile.module.css";
-import { useNavigate } from "react-router-dom";
 
-export default function ProjectTile({imgSrc, projectName, description, id}) {
-    const navigate = useNavigate();
-
+export default function ProjectTile({imgSrc, projectName, description, codeURL, vercelURL}) {
     return (
         <div className={classes.tileSection}>
             <img src={imgSrc} alt="project" className={classes.projectImg}/>
             <h2 className={classes.name}>{projectName}</h2>
             <a 
                 className={classes.detailBtn} 
-                href="https://youtube-clone-indol-zeta.vercel.app/"
+                href={vercelURL}
                 target="_blank">View Site</a>
             <a 
                 className={classes.detailBtn} 
-                href="https://nextjs-demo-pink-ten.vercel.app/"
+                href={codeURL}
                 target="_blank">View Code</a>
             <p>{description}</p>
         </div>
--- frontend/src/layout/ProjectSection.jsx
+++ frontend/src/layout/ProjectSection.jsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import classes from "./styles/ProjectSection.module.css";
 import ProjectTile from "../components/ProjectTile";
+
 export default function ProjectSection () {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/api/projects');
+                setProjects(response.data);
+                console.log(response.data)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, [])
+
     return (
         <div className={classes.projectTiles}>
-            <ProjectTile 
-                imgSrc="https://pbs.twimg.com/media/Fp6HcA0WwAAdF6J.jpg"
-                projectName="Youtube Clone"
-                description="Experience seamless browsing with interactive video tiles that autoplay on hover, giving you a sneak peek into each video. Our dynamic menu bar elegantly displays icons, expanding to reveal clear icon labels upon interaction. Utilizing the power of the YouTube API, our page fetches video data, allowing easy access to a wide array of content. Enhancing navigation, category tabs enable swift filtering for specific video genres. Plus, our intuitive search bar efficiently sifts through video results, ensuring a tailored viewing experience."
-                id="id1"/>
-            <ProjectTile 
-                imgSrc="https://media.idownloadblog.com/wp-content/uploads/2014/04/iOS-7-Weather-app-Adding.gif"
-                projectName="Weather App"
-                description="Discover and create memorable gatherings effortlessly with our meetup platform. Seamlessly organize events by adding new meetups through a user-friendly form, capturing crucial details like location, name, description, and images to entice attendees. Once added, these meetups take center stage on our main page, showcasing a comprehensive list of upcoming events. Easily peruse the available meetups and dive deeper into specifics with a convenient 'View Details' button, empowering users to explore event particulars at their convenience"
-                id= "id2"
-                />
+            {projects.map ( (project) => 
+                <ProjectTile 
+                    imgSrc={project.img}
+                    projectName={project.name}
+                    description={project.description}
+                    codeURL={project.codeURL}
+                    vercelURL={project.vercelURL}/>
+                )
+            }
         </div>
     )
 }
