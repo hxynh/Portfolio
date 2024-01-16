215d8417fdbed71c35979e55297faf4490f5bf2f
Nancy <nhoushop@outlook.com>
2024-01-16 01:13:02 -0500

    Deleted favicon and react logo

--- frontend/src/components/ProjectTile.jsx
+++ frontend/src/components/ProjectTile.jsx
@@ -16,22 +16,23 @@ export default function ProjectTile({imgSrc, projectName, description, codeURL, 
     return (
         <>
         <div className={classes.tileSection}>
-        <img src={imgSrc} alt="project" className={classes.projectImg} onClick={openModal}/>
-        <ProjectModal name={projectName}  isOpen={isModalOpen} onClose={closeModal}>
-        
-            
+            <img src={imgSrc} alt="project" className={classes.projectImg} onClick={openModal}/>
             <h2 className={classes.name}>{projectName}</h2>
-            <a 
-                className={classes.detailBtn} 
-                href={vercelURL}
-                target="_blank">View Site</a>
-            <a 
-                className={classes.detailBtn} 
-                href={codeURL}
-                target="_blank">View Code</a>
-            <p>{description}</p>
-        
-        </ProjectModal>
+            <ProjectModal 
+                name={projectName}  
+                isOpen={isModalOpen} 
+                onClose={closeModal}>
+                <h2 className={classes.name}>{projectName}</h2>
+                <a 
+                    className={classes.detailBtn} 
+                    href={vercelURL}
+                    target="_blank">View Site</a>
+                <a 
+                    className={classes.detailBtn} 
+                    href={codeURL}
+                    target="_blank">View Code</a>
+                <p>{description}</p>            
+            </ProjectModal>
         </div>
         </>
 
