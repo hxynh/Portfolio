b07cb9d0b9b5ae891415aacf3dda01414683c1c5
Nancy <nhoushop@outlook.com>
2024-02-07 22:59:31 -0500

    Added .env in backend and updated backend script

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@
 const mongoose = require ('mongoose')
 const dotenv = require ('dotenv').config()
 
-
+console.log(`URI: ${process.env.GMAIL_USER}`)
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log(`MongoDB connection: ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
