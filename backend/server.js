93d8b414adfa971b1fc399a1f8f6e919c1995fd0
Nancy <nhoushop@outlook.com>
2024-02-10 21:57:03 -0500

    Added CORS in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const dotenv = require ('dotenv').config()
+const cors = require ('cors')
 const port = process.env.PORT || 5000
 const {errorHandler} = require ('./middleware/errorMiddleware')
 const { connectDB } = require('./config/db')
@@ -8,6 +9,7 @@ connectDB()
 
 const app = express()
 
+app.use(cors())
 // Adding middleware to use body data
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
